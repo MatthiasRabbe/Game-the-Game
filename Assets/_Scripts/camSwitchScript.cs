5cfb7281434b41dfde9c1f30b9ad6342c015dd56
Angelo Selvaggio <a.selvaggio@gmx.de>
2021-02-02 09:14:34 +0100

    First world building

---
+++ Assets/_Scripts/camSwitchScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSwitchScript : MonoBehaviour
+{
+
+    private GameObject player;
+    public Camera buildingCam;
+    private GameObject cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        buildingCam.enabled = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            buildingCam.enabled = true;
+            cam.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //player.GetComponentInChildren<Camera>().enabled = true;
+            cam.SetActive(true);
+            buildingCam.enabled = false;
+        }
+    }
+}
